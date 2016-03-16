97f2a89ad15ef2c8ac4083c281fe4528955fa722
marcosocon <oconmarcos97@gmail.com>
2016-03-16 16:30:08 -0300

    added styles, functionality and removed compiled js

--- components/App.js
+++ components/App.js
@@ -1,16 +1,24 @@
 var React = require('react');
+
 var Search = require('./Search');
 var Map = require('./Map');
 var CurrentLocation = require('./CurrentLocation');
 var LocationList = require('./LocationList');
 
+
 var App = React.createClass({
+
   getInitialState(){
+
+    // Extract the favorite locations from local storage
+
     var favorites = [];
 
-    if (localStorage.favorites) {
+    if(localStorage.favorites){
       favorites = JSON.parse(localStorage.favorites);
     }
+
+    // Nobody would get mad if we center it on Paris by default
 
     return {
       favorites: favorites,
@@ -23,15 +31,18 @@ var App = React.createClass({
   },
 
   toggleFavorite(address){
-    if (this.isAddressInFavorites(address)){
+
+    if(this.isAddressInFavorites(address)){
       this.removeFromFavorites(address);
     }
     else{
-      this.addToFavorites
+      this.addToFavorites(address);
     }
+
   },
 
   addToFavorites(address){
+
     var favorites = this.state.favorites;
 
     favorites.push({
@@ -44,68 +55,103 @@ var App = React.createClass({
     });
 
     localStorage.favorites = JSON.stringify(favorites);
-
   },
 
   removeFromFavorites(address){
+
     var favorites = this.state.favorites;
     var index = -1;
 
-    for(var i = 0;i < favorites.length; i++){
-      if (favorites[i].address == address) {
+    for(var i = 0; i < favorites.length; i++){
+
+      if(favorites[i].address == address){
         index = i;
         break;
       }
+
     }
 
-    if (index!== -1) {
+    // If it was found, remove it from the favorites array
+
+    if(index !== -1){
+
       favorites.splice(index, 1);
 
       this.setState({
-        favorites: favorites;
+        favorites: favorites
       });
 
       localStorage.favorites = JSON.stringify(favorites);
     }
+
   },
 
-  isAddressOnFavorites(address){
+  isAddressInFavorites(address){
+
     var favorites = this.state.favorites;
 
-    for(var i = 0;i < favorites.length; i++){
-      if (favorites[i] == address) {
+    for(var i = 0; i < favorites.length; i++){
+
+      if(favorites[i].address == address){
         return true;
       }
+
     }
-      return false;
+
+    return false;
   },
 
   searchForAddress(address){
+
     var self = this;
 
-
-    // Using GMaps' geocode functionality
+    // We will use GMaps' geocode functionality,
+    // which is built on top of the Google Maps API
 
     GMaps.geocode({
       address: address,
-      callback: function(result, status){
-        if (status !== "OK") return;
+      callback: function(results, status) {
+
+        if (status !== 'OK') return;
+
         var latlng = results[0].geometry.location;
+
         self.setState({
           currentAddress: results[0].formatted_address,
-          mapCoordinates:{
+          mapCoordinates: {
             lat: latlng.lat(),
             lng: latlng.lng()
           }
-
         });
+
       }
     });
+
+  },
+
+  render(){
+
+    return (
+
+      <div>
+        <h1>Your Google Maps Locations</h1>
+
+        <Search onSearch={this.searchForAddress} />
+
+        <Map lat={this.state.mapCoordinates.lat} lng={this.state.mapCoordinates.lng} />
+
+        <CurrentLocation address={this.state.currentAddress}
+          favorite={this.isAddressInFavorites(this.state.currentAddress)}
+          onFavoriteToggle={this.toggleFavorite} />
+
+        <LocationList locations={this.state.favorites} activeLocationAddress={this.state.currentAddress}
+          onClick={this.searchForAddress} />
+
+      </div>
+
+    );
   }
 
-  //render.
+});
 
-
-})
-
-
+module.exports = App;
--- components/LocationList.js
+++ components/LocationList.js
@@ -1,25 +1,36 @@
 var React = require('react');
-var LocationItem = require.('./LocationItem');
+var LocationItem = require('./LocationItem');
 
 var LocationList = React.createClass({
+
   render(){
+
     var self = this;
 
     var locations = this.props.locations.map(function(l){
+
       var active = self.props.activeLocationAddress == l.address;
+
       // Notice that we are passing the onClick callback of this
       // LocationList to each LocationItem.
-      return < LocationItem address={l.address} timestamp={l.timestamp} active={active} onClick={self.props.onClick} />
 
+      return <LocationItem address={l.address} timestamp={l.timestamp}
+          active={active} onClick={self.props.onClick} />
     });
-    if (!location.length) {
+
+    if(!locations.length){
       return null;
     }
-    return(
+
+    return (
       <div className="list-group col-xs-12 col-md-6 col-md-offset-3">
-        <span className="list-group item-active"> Saved Locations</span>
+        <span className="list-group-item active">Saved Locations</span>
+        {locations}
       </div>
     )
+
   }
+
 });
+
 module.exports = LocationList;
